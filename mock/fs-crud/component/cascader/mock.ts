96ccce319ea5520cd71a07f2e88f095a4da559ec
xiaojunnuo <xiaojunnuo@qq.com>
2021-12-30 18:41:38 +0800

    refactor: 1

--- mock/fs-crud/component/cascader/mock.ts
+++ mock/fs-crud/component/cascader/mock.ts
@@ -7,15 +7,15 @@ const list = [
   {
     cascader: ['zhinan', 'shejiyuanze', 'yizhi'],
     lazyLoad: ['11', '1101', '110101', '110101001'],
-    multiple: ['antdv cascader不支持多选'],
+    multiple: [],
   },
   {
     cascader: ['zhinan', 'shejiyuanze', 'yizhi'],
-    multiple: ['antdv cascader不支持多选'],
+    multiple: [],
   },
   {
     cascader: ['zhinan', 'shejiyuanze', 'yizhi'],
-    multiple: ['antdv cascader不支持多选'],
+    multiple: [],
   },
 ];
 options.list = list;
