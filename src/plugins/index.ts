8b062e0e2088d1ced69d3806145c14f7c56bb162
xiaojunnuo <xiaojunnuo@qq.com>
2022-03-02 10:09:56 +0800

    refactor: element示例支持svg图标

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -4,3 +4,4 @@ export { setupCustomComponents } from '@/plugins/customComponents';
 export { setupGlobalMethods } from '@/plugins/globalMethods';
 export { setupFastCrud } from '@/plugins/fast_crud';
 import './iconify';
+import './iconfont';
