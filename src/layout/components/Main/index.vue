8f01a4b524c2856defb0c051709932767a4bbcd8
xiaojunnuo <xiaojunnuo@qq.com>
2022-10-25 13:16:12 +0800

    fix: 移除suspense组件，会导致上传组件（异步组件）不正常
    https://github.com/fast-crud/fast-crud/issues/101

--- src/layout/components/Main/index.vue
+++ src/layout/components/Main/index.vue
@@ -3,9 +3,7 @@
     <template #default="{ Component, route }">
       <transition :name="getTransitionName" mode="out-in" appear>
         <keep-alive v-if="keepAliveComponents" :include="keepAliveComponents">
-          <Suspense>
-            <component :is="Component" :key="route.fullPath" />
-          </Suspense>
+          <component :is="Component" :key="route.fullPath" />
         </keep-alive>
         <component v-else :is="Component" :key="route.fullPath" />
       </transition>
